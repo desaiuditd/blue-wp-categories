af4ab2378a5300a78a80698ed54162f21bd16131
Udit Desai <desaiuditd@gmail.com>
2017-07-28 13:53:53 -0700

    adds sync class

--- sync/class-bwpc_sync.php
+++ sync/class-bwpc_sync.php
@@ -6,7 +6,30 @@
  * Date: 7/28/17
  * Time: 1:50 PM
  */
-class BWPC_Sync
-{
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! class_exists( 'BWPC_Sync' ) ) {
+
+    /**
+     * Class BWPC_Sync
+     *
+     * Handles all the functionality to sync categories with external source.
+     *
+     * @since 0.1
+     */
+    class BWPC_Sync {
+
+        /**
+         * @since 0.1
+         */
+        function __construct() {
+
+        }
+
+    }
 
 }
